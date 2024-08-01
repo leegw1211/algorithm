22db5c0ac9b0e06976000a22d73aad9d7ec32198
이근우 <140172922+leegw1211@users.noreply.github.com>
2024-08-01 20:36:51 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -1,26 +1,24 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-void dfs(vector<int> &visited, vector<int> &answer, int size, int m, int latest) {
-	if (size == m) {
+
+int n, m;
+vector<int> answer;
+
+void dfs(int latest) {
+	if (answer.size() == m) {
 		for (int k : answer) cout << k << " ";
 		cout << "\n";
 		return;
 	}
-	for (int i=latest+1; i<visited.size(); i++) {
-		if (visited[i] == 0) {
-			answer.push_back(i);
-			visited[i] = 1;
-			dfs(visited, answer, size+1, m, i);
-			answer.pop_back();
-			visited[i] = 0;
-		}
+	for (int i=latest+1; i<=n; i++) {
+		answer.push_back(i);
+		dfs(i);
+		answer.pop_back();
 	}
 }
 
 int main() {
-	int n, m; cin >> n >> m;
-	vector<int> visited(n+1, 0);
-	vector<int> answer;
-	dfs(visited, answer, 0, m, 0);
+	cin >> n >> m;
+	dfs(0);
 }
